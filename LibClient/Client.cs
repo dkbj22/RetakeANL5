5adfd8a47b9ee2ccea69d1fe2da4163238f4c9c3
Tobias <Tobizelders@gmail.com>
2022-01-05 14:18:28 +0100

    Client commit

--- LibClient/Client.cs
+++ LibClient/Client.cs
@@ -7,6 +7,9 @@ using System.Text.Json;
 using System.Threading;
 // using LibData;
 using Microsoft.Extensions.Configuration;
+using System.Text;
+
+
 
 namespace LibClient
 {
@@ -15,7 +18,11 @@ namespace LibClient
         public int ServerPortNumber { get; set; }
         public string ServerIPAddress { get; set; }
 
+
+
     }
+
+
 
     public class Output
     {
@@ -27,16 +34,22 @@ namespace LibClient
         public string ReturnDate { get; set; } // the email of the borrower in case the status is borrowed, otherwise null
     }
 
+
+
     abstract class AbsSequentialClient
     {
         protected Setting settings;
 
+
+
         /// <summary>
-        /// Report method can be used to print message to console in standaard formaat. 
+        /// Report method can be used to print message to console in standaard formaat.
         /// It is not mandatory to use it, but highly recommended.
         /// </summary>
         /// <param name="type">For example: [Exception], [Error], [Info] etc</param>
         /// <param name="msg"> In case of [Exception] the message of the exection can be passed. Same is valud for other types</param>
+
+
 
         protected void report(string type, string msg)
         {
@@ -47,8 +60,12 @@ namespace LibClient
                 msg = msg.Replace(@"\u0022", " ");
             }
 
+
+
             Console.Out.WriteLine("[Client] {0} : {1}", type, msg);
         }
+
+
 
         /// <summary>
         /// This methid loads required settings.
@@ -60,9 +77,11 @@ namespace LibClient
             {
                 string path = AppDomain.CurrentDomain.BaseDirectory;
                 IConfiguration Config = new ConfigurationBuilder()
-                    .SetBasePath(Path.GetFullPath(Path.Combine(path, @"../../../../")))
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+                .SetBasePath(Path.GetFullPath(Path.Combine(path, @"../../../../")))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
 
                 settings.ServerIPAddress = Config.GetSection("ServerIPAddress").Value;
                 settings.ServerPortNumber = Int32.Parse(Config.GetSection("ServerPortNumber").Value);
@@ -71,11 +90,17 @@ namespace LibClient
             catch (Exception e) { report("[Exception]", e.Message); }
         }
 
+
+
         protected abstract void createSocketAndConnect();
         public abstract Output handleConntectionAndMessagesToServer();
         protected abstract Message processMessage(Message message);
 
+
+
     }
+
+
 
 
 
@@ -87,10 +112,14 @@ namespace LibClient
         public IPEndPoint serverEndPoint;
         public IPAddress ipAddress;
 
+
+
         public string client_id;
         private string bookName;
 
-        //This field is optional to use. 
+
+
+        //This field is optional to use.
         private int delayTime;
         /// <summary>
         /// Initializes the client based on the given parameters and seeting file.
@@ -101,12 +130,16 @@ namespace LibClient
         {
             GetConfigurationValue();
 
+
+
             // this.delayTime = 100;
             this.bookName = bookName;
             this.client_id = "Client " + id.ToString();
             this.result = new Output();
             result.Client_id = this.client_id;
         }
+
+
 
 
         /// <summary>
@@ -123,26 +156,61 @@ namespace LibClient
             Console.WriteLine("\n");
         }
 
+
+
         /// <summary>
         /// Connect socket settings and connect to the helpers.
         /// </summary>
         protected override void createSocketAndConnect()
         {
-             //todo: To meet the assignment requirement, finish the implementation of this method.
-  
-            // try
-            // {
-              
-            // }
-            // catch ()
-            // {
-             
-            // }
+
+            string stringToSent = "Client: Hello Server";
+
+
+
+            byte[] bytes = new byte[1024];
+            IPAddress ipAddress = IPAddress.Parse(settings.ServerIPAddress);
+            IPEndPoint remoteIp = new IPEndPoint(ipAddress, settings.ServerPortNumber);
+
+
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+
+            try
+            {
+                s.Connect(remoteIp);
+                Console.WriteLine($"Socket connected to {s.RemoteEndPoint.ToString()}");
+
+
+
+                byte[] msg = Encoding.ASCII.GetBytes(stringToSent);
+                int bytesSent = s.Send(msg);
+                int bytesRecieved = s.Receive(bytes);
+
+
+
+                string RecievedString = Encoding.ASCII.GetString(bytes, 0, bytesRecieved);
+
+
+
+                s.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
 
         }
 
+
+
         /// <summary>
-        /// This method starts the socketserver after initializion and handles all the communications with the server. 
+        /// This method starts the socketserver after initializion and handles all the communications with the server.
         /// Note: The signature of this method must not change.
         /// </summary>
         /// <returns>The final result of the request that will be written to output file</returns>
@@ -151,18 +219,24 @@ namespace LibClient
             this.report("starting:", this.client_id + " ; " + this.bookName);
             createSocketAndConnect();
 
+
+
             //todo: To meet the assignment requirement, finish the implementation of this method.
             // try
             // {
-               
-               
+
+
             // }
-            // catch () {  }
+            // catch () { }
+
+
 
             return this.result;
         }
 
-       
+
+
+
 
         /// <summary>
         /// Process the messages of the server. Depending on the logic, type and content of a message the client may return different message values.
@@ -175,12 +249,13 @@ namespace LibClient
             //todo: To meet the assignment requirement, finish the implementation of this method.
             // try
             // {
-               
+
             // }
-            // catch () {  }
+            // catch () { }
+
+
 
             return processedMsgResult;
         }
     }
 }
-
